7555d28684cecbc6b7f64aee2f00ac420ca1efb0
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2024-12-19 21:16:51 +0000

    started working on AccountService

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 /* If you would like to use a service on another computer simply copy and paste the folder
    and run npm i on it then node . in the root of the folder*/
 async function startServices() {
+    console.log(`
+                 █████╗ ██████╗  ██████╗ █████╗ ███╗   ██╗███████╗██╗   ██╗███████╗
+                ██╔══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║██╔════╝██║   ██║██╔════╝
+                ███████║██████╔╝██║     ███████║██╔██╗ ██║█████╗  ██║   ██║███████╗
+                ██╔══██║██╔══██╗██║     ██╔══██║██║╚██╗██║██╔══╝  ╚██╗ ██╔╝╚════██║
+                ██║  ██║██║  ██║╚██████╗██║  ██║██║ ╚████║███████╗ ╚████╔╝ ███████║
+                ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝  ╚═══╝  ╚══════╝`);
     require("./EpicGames/AccountService/index");
 }
 
